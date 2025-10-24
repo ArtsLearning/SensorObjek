afd4fc959bc77d9657b61d08c5b4da79fde223cc
MRifqy51 <rifqyhidayat51@gmail.com>
2025-10-24 14:39:36 +0700

    Apply updated .gitignore and remove tracked files

--- home/views.py
+++ home/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'home/index.html')
+def home(request):
+    """View untuk halaman home"""
+    context = {
+        'page_title': 'Home',
+    }
+    return render(request, 'home/index.html', context)
+
+def about(request):
+    """View untuk halaman about"""
+    context = {
+        'page_title': 'About Me',
+    }
+    return render(request, 'home/about.html', context)
+
+def gallery(request):
+    """View untuk halaman galeri"""
+    context = {
+        'page_title': 'Galeri',
+    }
+    return render(request, 'home/gallery.html', context)
